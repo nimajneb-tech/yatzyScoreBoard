1df6e16775f886eeb994580896dbc1f92e67e9ff
Benjamin <benjamin@housefofcode.io>
2021-07-26 11:55:42 +0200

    Add snapkit pod

---
+++ YatzyScorecard/Pods/Target Support Files/Pods-YatzyScorecard/Pods-YatzyScorecard-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YatzyScorecard : NSObject
+@end
+@implementation PodsDummy_Pods_YatzyScorecard
+@end
---
+++ YatzyScorecard/YatzyScorecard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YatzyScorecard
+//
+//  Created by Benjamin on 26/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
